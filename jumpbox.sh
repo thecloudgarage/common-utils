c30566e6e933fe9f6e115dfeb2754dc76a68e33a
thecloudgarage <39495790+thecloudgarage@users.noreply.github.com>
2020-08-27 12:00:11 +0530

    Create jumpbox.sh

---
+++ jumpbox.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install openjdk-8-jdk -y
+sudo apt install jq -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update -y
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo usermod -aG docker ubuntu
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+echo "done"
