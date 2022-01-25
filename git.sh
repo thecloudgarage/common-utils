38e3a7ed0ddee00c4be8b55322f2d92597a90ede
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-01-25 11:55:35 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git init
 git remote add <repo-name>  https://github.com/thecloudgarage/<repo-name>.git
+git branch <branch-name>
 git add .
-git commit -m 'new commit'
-git push -f <repo-name> master
+git commit -m 'branch-name-message'
+git push -f <repo-name> <branch-name>
