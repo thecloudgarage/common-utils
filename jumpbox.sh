0c87ff19d480e549c7db0fb308c46f56f41d86a1
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-01-25 07:52:46 +0530

    Update jumpbox.sh

--- jumpbox.sh
+++ jumpbox.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+sudo su
 sudo apt update -y
 sudo apt install openjdk-8-jdk -y
 sudo apt install jq -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update -y
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
-sudo usermod -aG docker ubuntu
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.7
+sudo apt update
+sudo apt install python3-pip
+pip3 install twine
+pip3 install --upgrade keyrings.alt
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/kubectl
